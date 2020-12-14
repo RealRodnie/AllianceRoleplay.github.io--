6e8359e80460084661d866d1a98c18480d5f1ebb
AllianceRoleplay <75799389+AllianceRoleplay@users.noreply.github.com>
2020-12-14 18:44:30 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -312,7 +312,7 @@ var settings = {
 //Donation
 
 let title = "Donation Goal for a new dedicated server";
-let currentDonations = 0;
+let currentDonations = 22;
 let currentGoal = 50;
 let percentageAchieved = (currentDonations / currentGoal) * 100;
 
